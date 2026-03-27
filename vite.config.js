5aef163f41a0d4e19c0424e888eb82cd5c24fdec
seantan338 <sean@sunriserecruit.com>
2026-03-28 03:28:18 +0800

    🔓 彻底解除 Host 访问限制

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: true // 允许所有地址访问，这样绑定域名时也不会报错了
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: true
+  }
 })
